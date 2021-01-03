42582cf3ebeeb0302ac61f587248e1842eb4c3cd
Khush Tated <khushtated@gmail.com>
2021-01-03 16:42:06 -0500

    front-end and basic JS implemented

--- popup.js
+++ popup.js
@@ -16,13 +16,46 @@ function handler(){
     })
     .then(data => {
         if(data[stock]==null){
+            document.getElementById("high").innerHTML = ""; 
+            document.getElementById("median").innerHTML = ""; 
+            document.getElementById("low").innerHTML = ""; 
+    
+    
+            document.getElementById("ticker").innerHTML = ""; 
+            document.getElementById("high-target").innerHTML = ""; 
+            document.getElementById("median-target").innerHTML = ""; 
+            document.getElementById("low-target").innerHTML = ""; 
+            document.getElementById("buy-sell").innerHTML = "Sorry, we couldn't find this stock"; 
             return; 
         }
-       
+
+        let rec = data[stock][0]["buysell"]; 
+        if(rec == "Sell"){
+            document.body.style.backgroundColor = "#FF9A8B"; 
+            document.body.style.backgroundImage = "linear-gradient(90deg, #FF9A8B 0%, #FF6A88 55%, #FF99AC 100%)";
+        }
+        if(rec == "Hold"){
+            document.body.style.backgroundColor = "#FBAB7E"; 
+            document.body.style.backgroundImage = "linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%)";
+        }
+        if(rec == "Buy"){
+            document.body.style.backgroundColor = "#85FFBD";
+            document.body.style.backgroundImage = "linear-gradient(45deg, #85FFBD 0%, #FFFB7D 100%)";
+        }
+
+        document.getElementById("high").innerHTML = "High: "; 
+        document.getElementById("median").innerHTML = "Median: "; 
+        document.getElementById("low").innerHTML = "Low: "; 
+
+
         document.getElementById("ticker").innerHTML = data[stock][0]["company"]; 
-        document.getElementById("high-target").innerHTML = "High Target: "+data[stock][0]["high"]; 
-        document.getElementById("median-target").innerHTML = "Median Target: "+data[stock][0]["median"]; 
-        document.getElementById("buy-sell").innerHTML = "Buy/Sell/Hold: "+data[stock][0]["buysell"]; 
+        document.getElementById("high-target").innerHTML = "$"+data[stock][0]["high"]; 
+        document.getElementById("median-target").innerHTML = "$"+data[stock][0]["median"]; 
+        document.getElementById("low-target").innerHTML = "$"+data[stock][0]["low"]; 
+
+        let analysts = data[stock][0]["analysts"]; 
+        document.getElementById("buy-sell").innerHTML = analysts+" Analysts Say: "+ data[stock][0]["buysell"]; 
+
 
     })
 }
